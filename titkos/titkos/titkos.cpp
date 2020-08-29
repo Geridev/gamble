d1a01a1b927a22f2a4ca5ca42dc0fce94ad83d4e
Geridev <69975941+Geridev@users.noreply.github.com>
2020-08-29 14:22:07 +0200

    Update titkos.cpp

--- titkos/titkos/titkos.cpp
+++ titkos/titkos/titkos.cpp
@@ -6,15 +6,24 @@ using namespace std;
 
 int main()
 {
+    int ossz = 50000;
+    int bet;
+    cout << "tedd fel az oszeget" << endl;
+    cin >> bet;
+    cout<< "osszeged: " <<ossz-bet<<endl;
+    int a;
+    cout << "adj meg egy szamot" << endl;
+    cin >> a;
     srand(time(0));
-    int a = rand() % 2; 
-	if (a==0)
-	{
-		cout << "Milan" << endl;
-	}
-	else
-	{
-		cout << "Pepe" << endl;
-	}
+    int b = rand() % 1; 
+    cout << b << endl;
+    if (a==b)
+    {
+        cout << "Gratulalok nyertel" << 2 * bet + ossz << endl;
+    }
+    else
+    {
+        cout << "sajna vesztetel" << endl;
+    }
+    cout << ossz << endl;
 }
-
